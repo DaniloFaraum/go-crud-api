bae1dd110075b7a7ad6a8cac17cb2f8acb3e11fc
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-08-31 21:17:06 -0300

    feat: add create operation to CRUD

--- controller/createPerson.go
+++ controller/createPerson.go
@@ -1,9 +1,37 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/DaniloFaraum/go-crud-api/schemas"
+	"github.com/gin-gonic/gin"
+)
 
 func CreatePersonHandler(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "placeholder",
-	})
+	request := CreatePersonRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	person := schemas.Person{
+		Name:      request.Name,
+		Age:       request.Age,
+		Gender:    request.Gender,
+		Ocupation: request.Ocupation,
+		Salary:    request.Salary,
+		Alive:     *request.Alive,
+	}
+
+	if err := db.Create(&person).Error; err != nil {
+		logger.Errorf("person request could not be created: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "could not create entry on db")
+		return
+	}
+
+	sendSucess(ctx, "create-person", person)
 }
