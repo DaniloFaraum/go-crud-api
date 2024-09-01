7ffca8dce1d0762724a1b8a9c8940b5de6bc7fa4
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-08-31 23:40:48 -0300

    feat: add list one operation to CRUD

--- controller/showPerson.go
+++ controller/showPerson.go
@@ -1,9 +1,28 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/DaniloFaraum/go-crud-api/schemas"
+	"github.com/DaniloFaraum/go-crud-api/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func ShowPersonHandler(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "placeholder",
-	})
+	id := ctx.Query("id")
+	if id == ""{
+		sendError(ctx, http.StatusBadRequest, errParamIsrequired("id","queryParam").Error())
+		return
+	}
+
+	person := schemas.Person{}
+
+	if err := db.First(&person, id).Error; err!=nil{
+		logger.Errorf("could not find person: %v", err)
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("person with id: %s not found", id))
+		return
+	}
+
+	sendSucess(ctx, "show-person", utils.ConvertToPersonResponse(person))
 }
