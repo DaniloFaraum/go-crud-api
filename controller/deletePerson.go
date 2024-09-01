f7204b2092d580fce95252a7461b87bfd4761beb
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-08-31 21:54:47 -0300

    feat: add delete operation to CRUD

--- controller/deletePerson.go
+++ controller/deletePerson.go
@@ -1,9 +1,31 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/DaniloFaraum/go-crud-api/schemas"
+	"github.com/gin-gonic/gin"
+)
 
 func DeletePersonHandler(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "placeholder",
-	})
+	id := ctx.Query("id")
+	if id == ""{
+		sendError(ctx, http.StatusBadRequest, errParamIsrequired("id","queryParam").Error())
+		return
+	}
+
+	person := schemas.Person{}
+
+	if err := db.First(&person, id).Error; err!=nil{
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("person with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&person).Error; err!=nil{
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("could not delete person with id: %s", id))
+		return
+	}
+
+	sendSucess(ctx, "delete-person", person)
 }
