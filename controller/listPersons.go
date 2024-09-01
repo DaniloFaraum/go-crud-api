6ab155448674f87e4d8c366b29cd2437561668c0
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-08-31 23:40:22 -0300

    feat: add list all operation to CRUD

--- controller/listPersons.go
+++ controller/listPersons.go
@@ -1,9 +1,20 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/DaniloFaraum/go-crud-api/schemas"
+	"github.com/DaniloFaraum/go-crud-api/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func ListPersonHandler(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "placeholder",
-	})
+	var persons []schemas.Person
+
+	if err := db.Find(&persons).Error; err!=nil{
+		logger.Errorf("could not find persons: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "could not get persons")
+		return
+	}
+	sendSucess(ctx, "list-persons", utils.ConvertToPersonResponses(persons))
 }
